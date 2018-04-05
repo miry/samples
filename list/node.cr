4a4fba74669b10387033e9655e6793d9809f964f
Michael Nikitochkin <miry.sof@gmail.com>
2018-04-05 15:16:42 +0200

    List: implementation on Crystal

---
+++ list/node.cr
@@ -0,0 +1,50 @@
+class Node
+  property value : String
+  property next : self | Nil
+
+  def initialize(@value : String?)
+  end
+
+  def append(node : self)
+    last : self = self
+    browse do |item|
+      last = item
+    end
+    last.next = node
+  end
+
+  def values
+    result = [] of String?
+    browse do |item|
+      result << item.value
+    end
+    result
+  end
+
+  def pop : self
+    prev : self = self
+    result : self = self
+    browse do |item|
+      prev, result = result, item
+    end
+    prev.next = nil
+    result
+  end
+
+  def size : Int32
+    result = 0
+    browse do
+      result += 1
+    end
+    result
+  end
+
+  def browse(&block)
+    last : self = self
+    yield(last)
+    while last.next
+      last = last.next.as(Node)
+      yield(last)
+    end
+  end
+end
