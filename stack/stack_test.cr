336e89ea28f3949a39bfe18716be2a7415bc2d20
Michael Nikitochkin <miry.sof@gmail.com>
2018-04-05 11:21:37 +0200

    Stack: Use cached object

--- stack/stack_test.cr
+++ stack/stack_test.cr
@@ -2,11 +2,15 @@ require "minitest/autorun"
 require "./stack"
 
 class StackTest < Minitest::Test
+  def stack
+    @stack ||= Stack.new
+  end
+
   def test_empty_on_init
-    assert_equal true, Stack.new.empty?
+    assert_equal true, stack.empty?
   end
 
   def test_no_items_on_init
-    assert_equal 0, Stack.new.size
+    assert_equal 0, stack.size
   end
 end
