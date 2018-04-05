81fc604e47f9446066e3c262c138b1211b83cf85
Michael Nikitochkin <miry.sof@gmail.com>
2018-04-05 15:46:47 +0200

    Stack: Added linked list stack implementation

---
+++ stack/stack_linked_list_test.cr
@@ -0,0 +1,43 @@
+require "minitest/autorun"
+require "./stack_linked_list"
+
+class StackLinkedListTest < Minitest::Test
+  def stack
+    @stack ||= StackLinkedList.new
+  end
+
+  def test_empty_on_init
+    assert_equal true, stack.empty?
+  end
+
+  def test_no_items_on_init
+    assert_equal 0, stack.size
+  end
+
+  def test_push_item
+    stack.push("foo")
+    assert_equal 1, stack.size
+  end
+
+  def test_pop_item
+    stack.push("foo")
+    assert_equal "foo", stack.pop()
+  end
+
+  def test_random_push_pop
+    stack.push("1")
+    stack.push("2")
+    assert_equal "2", stack.pop()
+    stack.push("3")
+    stack.push("4")
+    assert_equal "4", stack.pop()
+    assert_equal "3", stack.pop()
+    assert_equal "1", stack.pop()
+    assert_equal true, stack.empty?
+  end
+
+  def test_pop_empty_stack
+    assert_equal nil, stack.pop()
+    assert_equal true, stack.empty?
+  end
+end
