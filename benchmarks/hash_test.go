bdffcf0c4ecb6f0be96364bb33fe080c00d6db1c
Michael Nikitochkin <miry.sof@gmail.com>
2018-04-05 08:51:37 +0200

    Added benchmark random access hash and array

---
+++ benchmarks/hash_test.go
@@ -0,0 +1,37 @@
+package benchmarks
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func BenchmarkAccessStructure(b *testing.B) {
+	const size int = int(100000000)
+
+	var indexes = make([]int, size, size)
+	var arr = make([]int, size, size)
+	var hash = make(map[int]int)
+
+	rand.Seed(42)
+	for i := 0; i < size; i++ {
+		indexes[i] = rand.Intn(size)
+		arr[i] = i
+		hash[i] = i
+	}
+
+	b.ResetTimer()
+
+	b.Run("Array", func(b *testing.B) {
+		for i := 0; i < size; i++ {
+			indx := indexes[i]
+			_ = arr[indx]
+		}
+	})
+
+	b.Run("Hash", func(b *testing.B) {
+		for i := 0; i < size; i++ {
+			indx := indexes[i]
+			_ = hash[indx]
+		}
+	})
+}
