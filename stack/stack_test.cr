c979e960b123cdd4be96db8e8c1867bc2e4d23dc
Michael Nikitochkin <miry.sof@gmail.com>
2018-04-05 15:17:51 +0200

    Stack: Use Crystalls array implementation for stack.

--- stack/stack_test.cr
+++ stack/stack_test.cr
@@ -13,4 +13,29 @@ class StackTest < Minitest::Test
   def test_no_items_on_init
     assert_equal 0, stack.size
   end
+
+  def test_push_item
+    stack.push("foo")
+    assert_equal 1, stack.size
+  end
+
+  def test_pop_item
+    stack.push("foo")
+    assert_equal "foo", stack.pop()
+  end
+
+  def test_random_push_pop
+    stack.push("1")
+    stack.push("2")
+    assert_equal "2", stack.pop()
+    stack.push("3")
+    stack.push("4")
+    assert_equal "4", stack.pop()
+    assert_equal "3", stack.pop()
+    assert_equal "1", stack.pop()
+  end
+
+  def test_pop_empty_stack
+    assert_equal nil, stack.pop()
+  end
 end
