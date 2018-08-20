699a56e3089436030d2470210b34732422559fd7
Michael Nikitochkin <miry.sof@gmail.com>
2018-08-20 23:52:21 +0200

    SudokuSolver: Removed outputs

--- codewars/sudoku.rb
+++ codewars/sudoku.rb
@@ -1,22 +1,17 @@
 FULL = [1,2,3,4,5,6,7,8,9]
 
 def sudoku(board)
-  print board
-  puts "------"
-  print fill_board
+  fill_board(board)
 end
 
 def fill_board(board)
   result = clone board
-
-  9.times do |row|
-    9.times do |col|
-      cell = result[row][col]
-      p cell
-      p allow?(board: result, value: 1, row: 0, col: 0)
-    end
+  i = 1
+  while !valid?(result) do
+    result = clone result
+    i += 1
+    raise "Looping" if i == 1000
   end
-
   result
 end
 
@@ -25,7 +20,12 @@ def clone(board)
   9.times do |row|
     result[row] = Array.new 9
     9.times do |col|
-      result[row][col] = board[row][col] == 0 ? possible_cell(board: board, row: row, col: col) : board[row][col]
+      result[row][col] = case board[row][col]
+                          when 0, Array
+                            possible_cell(board: board, row: row, col: col)
+                         else
+                           board[row][col]
+                         end
     end
   end
   result
@@ -81,18 +81,19 @@ end
 
 def print(board)
   board.each do |row|
-    puts row.map {|i| i.is_a?(Array) ? 0 : i}.join(" ")
+    # puts row.map {|i| i.is_a?(Array) ? 0 : i}.join(" ")
+    puts row.map {|i| i.is_a?(Array) ? "#{i}" : i}.join(" ")
   end
 end
 
 def get_row(board, i)
-  board[i]
+  board[i].reject {|i| i.is_a? Array }
 end
 
 def get_col(board, i)
   result = Array.new 9
   9.times {|r| result[r] = board[r][i] }
-  result
+  result.reject {|i| i.is_a? Array }
 end
 
 def get_block(board, coord)
@@ -101,18 +102,14 @@ def get_block(board, coord)
 
   rows = board[first_row..last_row]
 
-  first_col = coord[0] * 3
+  first_col = coord[1] * 3
   last_col = first_col + 2
 
-  rows.map {|r| r[first_col..last_col]}.flatten
+  rows.map {|r| r[first_col..last_col].reject {|i| i.is_a? Array } }.flatten
 end
 
-def allow?(board:,value:,row:,col:)
-  !get_row(board, row).include?(value) &&
-  !get_col(board, col).include?(value) &&
-  !get_block(board, [row/3,col/3]).include?(value)
-end
-
-def possible_cell(board: board,row: row, col: col)
-  FULL - get_row(board, row) - get_col(board, col) - get_block(board, [row/3,col/3])
+def possible_cell(board: board, row: row, col: col)
+  result = FULL - get_row(board, row) - get_col(board, col) - get_block(board, [row/3,col/3])
+  return result.first if result.size == 1
+  result
 end
