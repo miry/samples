cc907b65b662ab1823527381076f33b9f0fa5819
Michael Nikitochkin <miry.sof@gmail.com>
2018-08-30 09:30:36 +0200

    Solve problem from Codingame (#2)
    * CodingGame: Started work on Gravity Tumbler
    
    * Parse input and added tests
    
    * Use bytes for punkts
    
    * Circleci workflow
    
    * Changed working directory
    
    * Run tests per folder
    
    * CI test results
    
    * Added input gravity
    
    * Test for one dimensions
    
    * REturn input on gravity and rotate
    
    * Print the field
    
    * Added more tests
    
    * Compare with fields

---
+++ codinggame/gravity_tumbler/README.md
@@ -0,0 +1,9 @@
+The program must output the result of tumbling a landscape a certain number of times.
+
+Tumbling entails:
+- rotating the landscape counterclockwise by 90°
+- letting the hash bits # fall down
+
+The map is composed of . and #.
+
+(This puzzle is a twist (hah!) on classic community puzzle “Gravity”. You may want to solve that one first.)
---
+++ codinggame/gravity_tumbler/input.go
@@ -0,0 +1,107 @@
+package gravity_tumbler
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Input struct {
+	Width  int
+	Height int
+	Count  int
+	Fields [][]byte
+}
+
+func NewInputFromReader(r *os.File) (*Input, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Buffer(make([]byte, 1000000), 1000000)
+
+	result := Input{}
+
+	scanner.Scan()
+	fmt.Sscan(scanner.Text(), &result.Width, &result.Height)
+
+	scanner.Scan()
+	fmt.Sscan(scanner.Text(), &result.Count)
+
+	result.Fields = make([][]byte, result.Height, result.Height)
+	for i := 0; i < result.Height; i++ {
+		scanner.Scan()
+		result.Fields[i] = scanner.Bytes()
+	}
+
+	return &result, nil
+}
+
+func (i *Input) Run() *Input {
+	table := i
+	for k := 0; k < i.Count; k++ {
+		table = table.Rotate()
+		table = table.Gravity()
+	}
+	return table
+}
+
+func (i *Input) Print() {
+	for k := 0; k < i.Height; k++ {
+		fmt.Println(string(i.Fields[k]))
+	}
+}
+
+func (i *Input) Gravity() *Input {
+	result := make([][]byte, i.Height)
+
+	for j := 0; j < i.Height; j++ {
+		result[j] = make([]byte, i.Width, i.Width)
+	}
+
+	for j := 0; j < i.Width; j++ {
+		light := countsOf(i.Fields, j, '.')
+		for k := 0; k < light; k++ {
+			result[k][j] = '.'
+		}
+		for k := light; k < i.Height; k++ {
+			result[k][j] = '#'
+		}
+	}
+
+	input := Input{
+		Width:  i.Width,
+		Height: i.Height,
+		Fields: result,
+	}
+
+	return &input
+}
+
+func (i *Input) Rotate() *Input {
+	result := make([][]byte, i.Width)
+
+	for j := 0; j < i.Width; j++ {
+		result[j] = make([]byte, i.Height, i.Height)
+	}
+
+	for j := 0; j < i.Width; j++ {
+		for k := 0; k < i.Height; k++ {
+			result[j][k] = i.Fields[k][j]
+		}
+	}
+
+	input := Input{
+		Width:  i.Height,
+		Height: i.Width,
+		Fields: result,
+	}
+	return &input
+}
+
+func countsOf(field [][]byte, col int, c byte) int {
+	result := 0
+	for _, row := range field {
+		if row[col] == c {
+			result++
+		}
+	}
+	return result
+}
