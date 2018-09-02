27ced7857ff6cf4257c4bfe8967e635cc0a0687b
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-02 13:36:34 +0200

    Algorithms: Go Heap via array insert

--- algorithms/heap/heap.go
+++ algorithms/heap/heap.go
@@ -1,19 +1,22 @@
 package heap
 
-// Heap is a specialized tree-based data structure that satisfies the heap property:
-// if P is a parent node of C, then the key (the value) of P is either greater
-// than or equal to (in a max heap) or less than or equal to (in a min heap) the key of C.
-// The node at the "top" of the heap (with no parents) is called the root node.
-// https://en.wikipedia.org/wiki/Heap_(data_structure)
+import "fmt"
+
 type heap struct {
 	values []int
 }
 
+// NewHeap returns data strucutures Heap. Heap is a specialized tree-based data structure that satisfies the heap property:
+// if P is a parent node of C, then the key (the value) of P is either greater
+// than or equal to (in a max heap) or less than or equal to (in a min heap) the key of C.
+// The node at the "top" of the heap (with no parents) is called the root node.
+// https://en.wikipedia.org/wiki/Heap_(data_structure)
 func NewHeap() *heap {
 	return &heap{
 		values: make([]int, 1, 10),
 	}
 }
+
 func (h *heap) Max() *int {
 	if h.Size() > 0 {
 		return &h.values[1]
@@ -27,5 +30,33 @@ func (h *heap) Size() int {
 
 func (h *heap) Insert(v int) error {
 	h.values = append(h.values, v)
+	h.swim(h.Size())
 	return nil
+}
+
+func (h *heap) Print() {
+	fmt.Printf("%+v\n", h.values)
+}
+
+// 0  1  2  3  4  5  6  7
+// 0 88
+//      51 34
+//            31 24 15 22
+func (h *heap) swim(k int) {
+	h.Print()
+	for k > 1 && h.less(k/2, k) {
+		h.exch(k, k/2)
+		k = k / 2
+		h.Print()
+	}
+}
+
+func (h *heap) less(i, j int) bool {
+	return h.values[i] < h.values[j]
+}
+
+func (h *heap) exch(i, j int) {
+	s := h.values[i]
+	h.values[i] = h.values[j]
+	h.values[j] = s
 }
--- algorithms/heap/heap_test.go
+++ algorithms/heap/heap_test.go
@@ -8,20 +8,42 @@ import (
 	"github.com/miry/samples/algorithms/heap"
 )
 
-func TestHeap(t *testing.T) {
+func TestHeapMaxNil(t *testing.T) {
 	heap := heap.NewHeap()
 	assert.Nil(t, heap.Max())
 }
 
-func TestHeapSize(t *testing.T) {
+func TestHeapSizeEmpty(t *testing.T) {
 	heap := heap.NewHeap()
 	assert.Equal(t, 0, heap.Size())
 }
 
-func TestHeapInsert(t *testing.T) {
+func TestHeapInsertOneElement(t *testing.T) {
 	heap := heap.NewHeap()
 	assert.Equal(t, 0, heap.Size())
 
-	assert.NoError(t, heap.Insert(1))
+	assert.NoError(t, heap.Insert(83))
 	assert.Equal(t, 1, heap.Size())
+	assert.Equal(t, 83, *heap.Max())
+}
+
+func TestHeapInsertTwoElements(t *testing.T) {
+	heap := heap.NewHeap()
+	assert.Equal(t, 0, heap.Size())
+
+	assert.NoError(t, heap.Insert(83))
+	assert.NoError(t, heap.Insert(122))
+	assert.Equal(t, 2, heap.Size())
+	assert.Equal(t, 122, *heap.Max())
+}
+
+func TestHeapInsertThreeElements(t *testing.T) {
+	heap := heap.NewHeap()
+	assert.Equal(t, 0, heap.Size())
+
+	assert.NoError(t, heap.Insert(83))
+	assert.NoError(t, heap.Insert(122))
+	assert.NoError(t, heap.Insert(255))
+	assert.Equal(t, 3, heap.Size())
+	assert.Equal(t, 255, *heap.Max())
 }
