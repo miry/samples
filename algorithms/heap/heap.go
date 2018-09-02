2564272969b5368b5d8966d4b34b5c1cba53c7cd
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-02 14:04:11 +0200

    Algorithms: Go Heap sort.

--- algorithms/heap/heap.go
+++ algorithms/heap/heap.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type heap struct {
 	values []int
+	count  int
 }
 
 // NewHeap returns data strucutures Heap. Heap is a specialized tree-based data structure that satisfies the heap property:
@@ -13,24 +14,38 @@ type heap struct {
 // https://en.wikipedia.org/wiki/Heap_(data_structure)
 func NewHeap() *heap {
 	return &heap{
+		count:  0,
 		values: make([]int, 1, 10),
 	}
 }
 
 func (h *heap) Max() *int {
-	if h.Size() > 0 {
+	if h.count > 0 {
 		return &h.values[1]
 	}
 	return nil
 }
 
+func (h *heap) DeleteMax() *int {
+	max := h.Max()
+	if max == nil {
+		return nil
+	}
+	result := *max
+	h.exch(1, h.count)
+	h.count--
+	h.sink(1)
+	return &result
+}
+
 func (h *heap) Size() int {
-	return len(h.values) - 1
+	return h.count
 }
 
 func (h *heap) Insert(v int) error {
 	h.values = append(h.values, v)
-	h.swim(h.Size())
+	h.count++
+	h.swim(h.count)
 	return nil
 }
 
@@ -47,6 +62,27 @@ func (h *heap) swim(k int) {
 	for k > 1 && h.less(k/2, k) {
 		h.exch(k, k/2)
 		k = k / 2
+		h.Print()
+	}
+}
+
+// 0  1  2  3  4  5  6  7
+// 0 88
+//      51 34
+//            31 24 15 22
+func (h *heap) sink(k int) {
+	h.Print()
+	var child int
+	for k*2 <= h.count {
+		child = k * 2
+		if child < h.count && h.less(child, child+1) {
+			child++
+		}
+		if !h.less(k, child) {
+			break
+		}
+		h.exch(k, child)
+		k = child
 		h.Print()
 	}
 }
