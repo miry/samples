9c91a44c107a0141d7af4d80aaa3e3d9daa9f3e5
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-06 18:03:38 +0200

    Go: BTRee added count

--- algorithms/btree/tree_test.go
+++ algorithms/btree/tree_test.go
@@ -18,11 +18,13 @@ func TestTree(t *testing.T) {
 func TestTreeValuesWide(t *testing.T) {
 	wide := sample1()
 	assert.Equal(t, []int{1, 1, 2, 3, 5, 8, 13}, wide.Values())
+	assert.Equal(t, 7, wide.Size())
 }
 
 func TestTreeValuesDepth(t *testing.T) {
 	depth := sample2()
 	assert.Equal(t, []int{1, 1, 2, 3, 5, 8, 13}, depth.Values())
+	assert.Equal(t, 7, depth.Size())
 }
 
 func TestTreeValuesStreamWide(t *testing.T) {
@@ -48,27 +50,43 @@ func TestTreesEqual(t *testing.T) {
 	assert.True(t, wide.Equal(depth))
 }
 
+func TestPut(t *testing.T) {
+	wide := sample1()
+	assert.Equal(t, 7, wide.Size())
+	wide.Put(4)
+	assert.Equal(t, []int{4, 1, 1, 2, 3, 5, 8, 13}, wide.Values())
+	assert.Equal(t, 8, wide.Size())
+
+}
+
 func sample1() *btree.Tree {
 	head := &btree.Tree{
 		Left: &btree.Tree{
+			Count: 3,
 			Left: &btree.Tree{
 				Value: 1,
+				Count: 1,
 			},
 			Right: &btree.Tree{
 				Value: 2,
+				Count: 1,
 			},
 			Value: 1,
 		},
 		Right: &btree.Tree{
+			Count: 3,
 			Left: &btree.Tree{
 				Value: 5,
+				Count: 1,
 			},
 			Right: &btree.Tree{
 				Value: 13,
+				Count: 1,
 			},
 			Value: 8,
 		},
 		Value: 3,
+		Count: 7,
 	}
 	return head
 }
@@ -79,21 +97,28 @@ func sample2() *btree.Tree {
 			Left: &btree.Tree{
 				Left: &btree.Tree{
 					Value: 1,
+					Count: 1,
 				},
 				Right: &btree.Tree{
 					Value: 2,
+					Count: 1,
 				},
 				Value: 1,
+				Count: 3,
 			},
 			Right: &btree.Tree{
 				Value: 5,
+				Count: 1,
 			},
 			Value: 3,
+			Count: 2,
 		},
 		Right: &btree.Tree{
 			Value: 13,
+			Count: 1,
 		},
 		Value: 8,
+		Count: 4,
 	}
 	return head
 }
