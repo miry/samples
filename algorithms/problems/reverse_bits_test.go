505de3f697c7e03b64733b147c2737ea6357513d
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-08 20:15:49 +0200

    Solve problem Reverse Bits via Go

---
+++ algorithms/problems/reverse_bits_test.go
@@ -0,0 +1,31 @@
+package problems_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/miry/samples/algorithms/problems"
+)
+
+func TestReverseBits(t *testing.T) {
+	tests := []map[string]uint8{
+		map[string]uint8{
+			"in":  0xAB,
+			"out": 0xD5,
+		},
+		map[string]uint8{
+			"in":  0xFF,
+			"out": 0xFF,
+		},
+		map[string]uint8{
+			"in":  0x00,
+			"out": 0x00,
+		},
+	}
+
+	for _, test := range tests {
+		actual := problems.ReverseBits(test["in"])
+		assert.Equal(t, test["out"], actual)
+	}
+}
