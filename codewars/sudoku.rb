497588bfbd8c90a933a49bdd3c9532017f7077ff
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-09 14:59:04 +0200

    Sudoku updated print function

--- codewars/sudoku.rb
+++ codewars/sudoku.rb
@@ -1,44 +1,80 @@
-FULL = [1,2,3,4,5,6,7,8,9]
+# frozen_string_literal: true
+
+class NoChangesException < RuntimeError
+  attr_reader :board
+  def initialize(board, msg = 'No changes')
+    @board = board
+    super(msg)
+  end
+end
+
+FULL = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
 
 def sudoku(board)
+  puts 'Original: '
+  print_board(board)
   fill_board(board)
 end
 
 def fill_board(board)
   result = clone board
   i = 1
-  while !valid?(result) do
+  until valid?(result)
     result = clone result
+    puts "\n#{i}: "
+    print_board(result)
+
     i += 1
-    raise "Looping" if i == 1000
+    raise 'Looping' if i == 1000
+    break if i > 2
   end
   result
+rescue NoChangesException => e
+  puts '=== NO CHANGES :'
+  puts '=== ORGINAL :'
+  print_board(result)
+  puts '=== RESULT :'
+  print_board(e.board)
 end
 
 def clone(board)
   result = Array.new 9
+  found = false
   9.times do |row|
     result[row] = Array.new 9
     9.times do |col|
       result[row][col] = case board[row][col]
-                          when 0, Array
-                            possible_cell(board: board, row: row, col: col)
+                         when 0
+                           r = possible_cell(board: board, row: row, col: col, current: FULL)
+                           found = true unless r == 0
+                           r
+                         when Array
+                           r = possible_cell(board: board, row: row, col: col, current: board[row][col])
+                           if r.is_a?(Array)
+                             diff = board[row][col] - r
+                             puts "[#{row},#{col}]: #{board[row][col]} => #{r} : Diff: #{diff}"
+                             found = true unless diff.empty?
+                           else
+                             found = true
+                           end
+                           r
                          else
                            board[row][col]
                          end
     end
   end
+  raise NoChangesException.new(result, 'No changes') unless found
   result
 end
 
-def fill_row(row, permutation_index=1)
+def fill_row(row, permutation_index = 1)
   result = Array.new(9)
   subject = unused(row.sort).permutation.first(permutation_index).last
   row.each_with_index do |v, i|
     result[i] = if v == 0
-        subject.shift
-      else
-        v
+                  subject.shift
+                else
+                  v
       end
   end
   result
@@ -59,7 +95,7 @@ def valid?(board)
 
   3.times do |i|
     3.times do |j|
-      valid_block?(board, [i,j]) || (return false)
+      valid_block?(board, [i, j]) || (return false)
     end
   end
 
@@ -72,28 +108,37 @@ def valid_row?(row)
 end
 
 def valid_col?(board, col)
-  valid_row? board.map {|row| row[col] }
+  valid_row? board.map { |row| row[col] }
 end
 
 def valid_block?(board, coord)
   valid_row?(get_block(board, coord))
 end
 
-def print(board)
-  board.each do |row|
-    # puts row.map {|i| i.is_a?(Array) ? 0 : i}.join(" ")
-    puts row.map {|i| i.is_a?(Array) ? "#{i}" : i}.join(" ")
+def print_board(board)
+  board.each_slice(3) do |rows|
+    puts '-' * 180
+    rows.each do |row|
+      row.each_slice(3) do |cols|
+        cols.map do |cell|
+          print format('%17s', (cell.is_a?(Array) ? cell.to_s : cell))
+        end
+        print '  |  '
+      end
+      puts
+    end
   end
+  puts '-' * 180
 end
 
 def get_row(board, i)
-  board[i].reject {|i| i.is_a? Array }
+  board[i].reject { |i| i.is_a? Array }
 end
 
 def get_col(board, i)
   result = Array.new 9
-  9.times {|r| result[r] = board[r][i] }
-  result.reject {|i| i.is_a? Array }
+  9.times { |r| result[r] = board[r][i] }
+  result.reject { |i| i.is_a? Array }
 end
 
 def get_block(board, coord)
@@ -105,11 +150,16 @@ def get_block(board, coord)
   first_col = coord[1] * 3
   last_col = first_col + 2
 
-  rows.map {|r| r[first_col..last_col].reject {|i| i.is_a? Array } }.flatten
+  rows.map { |r| r[first_col..last_col].reject { |i| i.is_a? Array } }.flatten
 end
 
-def possible_cell(board: board, row: row, col: col)
-  result = FULL - get_row(board, row) - get_col(board, col) - get_block(board, [row/3,col/3])
+def get_dups_per_block_in_line(_board, _coord)
+  []
+end
+
+def possible_cell(board:, row:, col:, current: 0)
+  existing = current == 0 ? FULL : current
+  result = existing - get_row(board, row) - get_col(board, col) - get_block(board, [row / 3, col / 3])
   return result.first if result.size == 1
   result
 end
