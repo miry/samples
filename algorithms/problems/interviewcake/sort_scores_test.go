a224e9d69a08d351cbb0d23c50b18e0e0adfc3cc
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-10 09:41:53 +0200

    Added problem for sort scores with Go base on counter sort.

--- algorithms/problems/interviewcake/sort_scores_test.go
+++ algorithms/problems/interviewcake/sort_scores_test.go
@@ -1,43 +1,43 @@
-package problems_test
+package interviewcake_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/miry/samples/algorithms/problems"
+	problems "github.com/miry/samples/algorithms/problems/interviewcake"
 )
 
-func TestGetMaxProfit(t *testing.T) {
+func TestSortScores(t *testing.T) {
 	tests := []map[string][]int{
 		map[string][]int{
 			"in":  []int{},
-			"out": []int{0},
+			"out": []int{},
 		},
 		map[string][]int{
 			"in":  []int{1, 2},
-			"out": []int{1},
+			"out": []int{1, 2},
 		},
 		map[string][]int{
 			"in":  []int{2, 1},
-			"out": []int{0},
+			"out": []int{1, 2},
 		},
 		map[string][]int{
 			"in":  []int{10, 7, 5, 8, 11, 9},
-			"out": []int{6},
+			"out": []int{5, 7, 8, 9, 10, 11},
 		},
 		map[string][]int{
 			"in":  []int{10, 7, 5, 8, 11, 1, 2},
-			"out": []int{6},
+			"out": []int{1, 2, 5, 7, 8, 10, 11},
 		},
 		map[string][]int{
 			"in":  []int{10, 7, 45, 8, 11, 20, 30},
-			"out": []int{38},
+			"out": []int{7, 8, 10, 11, 20, 30, 45},
 		},
 	}
 
 	for _, test := range tests {
-		actual := problems.GetMaxProfit(test["in"])
-		assert.Equal(t, test["out"][0], actual, "Expected %v for %v", test["out"][0], test["in"])
+		actual := problems.SortScores(test["in"], 100)
+		assert.Equal(t, test["out"], actual, "Expected %v for %v", test["out"], test["in"])
 	}
 }
