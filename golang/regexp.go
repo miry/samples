6833846b6d6d82465b1167047fe83008f7831b25
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-13 21:13:50 +0200

    Golang added sample regexp.

---
+++ golang/regexp.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	// Compile the expression once, usually at init time.
+	// Use raw strings to avoid having to quote the backslashes.
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`)
+
+	fmt.Println(validID.MatchString("adam[23]"))
+	fmt.Println(validID.MatchString("eve[7]"))
+	fmt.Println(validID.MatchString("Job[48]"))
+	fmt.Println(validID.MatchString("snakey"))
+}
