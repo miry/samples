297b0d25387cad03cc9033211fa3186c0ff9ff8a
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-16 13:03:21 +0200

    Benchmarks tested for golang access to items for Hash vs Array.

--- benchmarks/hash_test.go
+++ benchmarks/hash_test.go
@@ -1,13 +1,21 @@
 package benchmarks
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
 
+/*
+BenchmarkAccessStructure show compare metrics between data strucuture and number of items.
+*/
 func BenchmarkAccessStructure(b *testing.B) {
-	const size int = int(100)
+	for _, size := range []int{1, 10, 100, 1000, 10000, 100000, 1000000} {
+		benchmarkAccessStructure(b, size)
+	}
+}
 
+func benchmarkAccessStructure(b *testing.B, size int) {
 	var indexes = make([]int, size, size)
 	var arr = make([]int, size, size)
 	var hash = make(map[int]int)
@@ -21,16 +29,16 @@ func BenchmarkAccessStructure(b *testing.B) {
 
 	b.ResetTimer()
 
-	b.Run("Array", func(b *testing.B) {
+	b.Run(fmt.Sprintf("Array_%d", size), func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			indx := indexes[i]
+			indx := indexes[i%size] % size
 			_ = arr[indx]
 		}
 	})
 
-	b.Run("Hash", func(b *testing.B) {
+	b.Run(fmt.Sprintf("Hash_%d", size), func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			indx := indexes[i]
+			indx := indexes[i%size] % size
 			_ = hash[indx]
 		}
 	})
