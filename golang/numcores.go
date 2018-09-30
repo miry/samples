f90554aed8f78f56ad845ad1b5dbb23fb48635e9
Michael Nikitochkin <miry.sof@gmail.com>
2018-09-30 11:01:35 +0200

    Golang: Samples how to get cores and debug app.

---
+++ golang/numcores.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+
+	// NumCPU returns the number of logical
+	// CPUs usable by the current process.
+	fmt.Println(runtime.NumCPU())
+}
