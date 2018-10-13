5cba85e753ce939b76fc2eaf915612c16552a878
Michael Nikitochkin <miry.sof@gmail.com>
2018-10-13 11:45:57 +0200

    JustforFunc: 10 context impl added value context impl.

--- justforfunc/10-contextimpl/context_test.go
+++ justforfunc/10-contextimpl/context_test.go
@@ -105,3 +105,36 @@ func TestWithTimeout(t *testing.T) {
 		t.Errorf("error should still be DeadlineExceeded, got %v", err)
 	}
 }
+
+func TestWithValue(t *testing.T) {
+	tc := []struct {
+		key, val, keyRet, valRet interface{}
+		shouldPanic              bool
+	}{
+		{"a", "b", "a", "b", false},
+		{"a", "b", "c", nil, false},
+		{42, true, 42, true, false},
+		{42, true, int64(42), nil, false},
+		{nil, true, nil, nil, true},
+		{[]int{1, 2, 3}, true, []int{1, 2, 3}, nil, true},
+	}
+
+	for _, tt := range tc {
+		var panicked interface{}
+		func() {
+			defer func() { panicked = recover() }()
+
+			ctx := WithValue(Background(), tt.key, tt.val)
+			if val := ctx.Value(tt.keyRet); val != tt.valRet {
+				t.Errorf("expected value %v, got %v", tt.valRet, val)
+			}
+		}()
+
+		if panicked != nil && !tt.shouldPanic {
+			t.Errorf("unexpected panic: %v", panicked)
+		}
+		if panicked == nil && tt.shouldPanic {
+			t.Errorf("expected panic, but didn't get it")
+		}
+	}
+}
