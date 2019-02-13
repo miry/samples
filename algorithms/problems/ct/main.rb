a9c372f7be7478654b0731624c914139b219d6f7
Michael Nikitochkin <miry.sof@gmail.com>
2019-02-13 08:38:20 +0100

    More tasks

--- algorithms/problems/ct/main.rb
+++ algorithms/problems/ct/main.rb
@@ -26,3 +26,58 @@ def sort(arr)
   end
   result.flatten
 end
+
+
+def subs(s, t)
+  words = s.split(' ')
+  existings_words = t.split(' ')
+
+  result = []
+  eindex = 0
+  windex = 0
+  while eindex < existings_words.size
+    while windex < words.size
+      word = words[windex]
+      if word != existings_words[eindex]
+        result << word
+      else
+        eindex += 1
+      end
+      windex += 1
+    end
+  end
+  result
+end
+
+
+def fourthBit(num)
+  num.to_s(2)[-4].to_i
+  num >> 3 & 1
+end
+
+
+def kSub(k, nums)
+  result = 0
+  i = 0
+  cache = {}
+  while i < nums.size
+    result += recursionKSub(k, nums[i..-1], cache)
+    i += 1
+  end
+  result
+end
+
+
+def recursionKSub(k, nums, cache={})
+  return cache[nums] if cache.key?(nums)
+  result = 0
+  sum = 0
+  nums.each do |num|
+    sum += num
+    if sum % k == 0
+      result += 1
+    end
+  end
+  cache[nums] = result
+  result
+end
--- algorithms/problems/ct/main_test.rb
+++ algorithms/problems/ct/main_test.rb
@@ -10,4 +10,33 @@ class TestSolution < Minitest::Test
     assert_equal [8, 4, 4, 1, 1, 1, 5, 5, 5, 5], sort([8, 5, 5, 5, 5, 1, 1, 1, 4, 4])
   end
 
+  def test_sub_sample
+    assert_equal %w[I using programming], subs("I am using HackerRank to improve programming", "am HackerRank to improve")
+  end
+
+  def test_sub_sample_two
+    assert_equal %w[I using am programming], subs("I am using am HackerRank to improve programming", "am HackerRank to improve")
+  end
+
+  def test_forbit_sample
+    assert_equal 0, fourthBit(32)
+    assert_equal 0, fourthBit(23)
+  end
+
+  def test_rec_k_sub
+    assert_equal 1, kSub(5, [5])
+    assert_equal 3, kSub(5, [5, 10])
+    assert_equal 3, kSub(5, [5, 10, 11])
+    assert_equal 1, kSub(5, [11, 9])
+    assert_equal 3, kSub(5, [10, 11, 9])
+    assert_equal 6, kSub(5, [5, 10, 11, 9])
+    assert_equal 10,kSub(5, [5, 10, 11, 9, 5])
+  end
+
+  def test_rec_ks
+    assert_equal 1, recursionKSub(5, [5])
+    assert_equal 1, recursionKSub(5, [10])
+    assert_equal 2, recursionKSub(5, [5, 10])
+    assert_equal 2, recursionKSub(5, [5, 10, 11])
+  end
 end
