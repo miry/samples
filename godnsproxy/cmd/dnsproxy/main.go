dd8bcfe9de594c3a1f00b22a43591087d1e6ef4f
Michael Nikitochkin <miry.sof@gmail.com>
2019-03-16 19:57:02 +0100

    godnsproxy: enable tls 1.3

--- godnsproxy/cmd/dnsproxy/main.go
+++ godnsproxy/cmd/dnsproxy/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "os"
+
 func main() {
 
+}
+
+func init() {
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 }
--- godnsproxy/cmd/dnstls/main.go
+++ godnsproxy/cmd/dnstls/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	server       = flag.String("server", "1.1.1.1", "DNS upstream server")
+	server       = flag.String("server", "1.1.1.1:853", "DNS upstream server")
 	printVersion = flag.Bool("version", false, "Print version")
 )
 
@@ -38,4 +38,8 @@ func main() {
 	for _, name := range names {
 		fmt.Printf("%s : %s\n", name, resolver.Lookup(name))
 	}
+}
+
+func init() {
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 }
