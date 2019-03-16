d4ff5c74c925bbbc7f6fdad446b817ee7f84937a
Michael Nikitochkin <miry.sof@gmail.com>
2019-03-16 19:58:03 +0100

    godnsproxy: cover lookup by sample test

--- godnsproxy/pkg/dnstls/resolver.go
+++ godnsproxy/pkg/dnstls/resolver.go
@@ -2,26 +2,27 @@ package dnstls
 
 import (
 	"errors"
-	"net"
 )
 
-// DNSTLS resolves domain names against nameserver with tls enabled
+// Resolver resolves domain names against nameserver with tls enabled
 type Resolver struct {
-	Server net.Addr
+	Server string
 }
 
-var NoServer = errors.New("server could not be empty")
+var errNoServer = errors.New("server could not be empty")
 
+// New returns  Resolver
 func New(server string) (*Resolver, error) {
 	result := &Resolver{}
 
 	if len(server) == 0 {
-		return result, NoServer
+		return result, errNoServer
 	}
 
 	return result, nil
 }
 
-func (r *Resolver) Lookup(name string) string {
-	return "127.0.0.1"
+// Lookup resolve name to ip address
+func (r *Resolver) Lookup(name string) (string, error) {
+	return "8.8.8.8", nil
 }
--- godnsproxy/pkg/dnstls/resolver_test.go
+++ godnsproxy/pkg/dnstls/resolver_test.go
@@ -8,10 +8,41 @@ import (
 	"github.com/miry/samples/godnsproxy/pkg/dnstls"
 )
 
-func TestNewResolver(t *testing.T) {
+func TestNewResolverWithError(t *testing.T) {
 	assert := assert.New(t)
 
-	actual, err := dnstls.New("")
-	assert.NotNil(actual)
-	assert.EqualError(err, "server could not be empty")
+	tc := []struct {
+		server string
+		err    string
+	}{
+		{"", "server could not be empty"},
+	}
+
+	for _, tt := range tc {
+		actual, err := dnstls.New(tt.server)
+		assert.NotNil(actual)
+		assert.EqualError(err, tt.err)
+	}
+}
+
+func TestLookup(t *testing.T) {
+	assert := assert.New(t)
+
+	subject, err := dnstls.New("1.1.1.1:853")
+	assert.Nil(err)
+
+	tc := []struct {
+		name     string
+		expected string
+	}{
+		{"google-public-dns-a.google.com", "8.8.8.8"},
+		{"google-public-dns-b.google.com", "8.8.4.4"},
+		{"one.one.one.one", "1.1.1.1"},
+	}
+
+	for _, tt := range tc {
+		actual, err := subject.Lookup(tt.name)
+		assert.Nil(err)
+		assert.Equal(tt.expected, actual)
+	}
 }
