ef4781b7ef4b4410a7c2737d71ba86e76bb19047
Michael Nikitochkin <miry.sof@gmail.com>
2019-12-03 23:38:51 +0100

    Advent of Code 2019: Day 3 Part 2: Accomplish

--- algorithms/problems/advent/2019/day_three.cr
+++ algorithms/problems/advent/2019/day_three.cr
@@ -45,7 +45,39 @@
 #
 # What is the Manhattan distance from the central port to the closest intersection?
 
-alias Coord = Tuple(Int32, Int32)
+# --- Part Two ---
+#
+# It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+#
+# To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection.
+#
+# The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+#
+# ...........
+# .+-----+...
+# .|.....|...
+# .|..+--X-+.
+# .|..|..|.|.
+# .|.-X--+.|.
+# .|..|....|.
+# .|.......|.
+# .o-------+.
+# ...........
+#
+# In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+#
+# However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+#
+# Here are the best steps for the extra examples from above:
+#
+#     R75,D30,R83,U83,L12,D49,R71,U7,L72
+#     U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+#     R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+#     U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+#
+# What is the fewest combined steps the wires must take to reach an intersection?
+
+alias Coord = Tuple(Int32, Int32, Int32)
 alias Line = Tuple(Coord, Coord)
 
 class Wire
@@ -55,7 +87,7 @@ class Wire
   def initialize(commands)
     @horizonatls = Set(Line).new
     @verticals = Set(Line).new
-    @start_position = {0, 0}
+    @start_position = {0, 0, 0}
 
     add(commands)
   end
@@ -64,7 +96,7 @@ class Wire
     position = @start_position
     commands.each do |command|
       x, y = step(command)
-      new_position = {position[0] + x, position[1] + y}
+      new_position = {position[0] + x, position[1] + y, position[2] + x.abs + y.abs}
       if x == 0
         @verticals << {position, new_position}
       else
@@ -75,39 +107,25 @@ class Wire
   end
 
   def intersection(other : Wire)
+    result = self.class.intersection_vertical(self, other)
+    result += self.class.intersection_vertical(other, self)
+    result
+  end
+
+  def self.intersection_vertical(wire, other)
     result = Set(Coord).new
-    # puts @verticals
-    # puts @horizonatls
-    @verticals.each do |vline|
+    wire.verticals.each do |vline|
       x = vline[0][0]
       y1, y2 = [vline[0][1], vline[1][1]].sort
 
-      # p "vline: #{vline} #{x} #{y1} #{y2}"
       other.horizonatls.each do |hline|
         y = hline[0][1]
         x1, x2 = [hline[0][0], hline[1][0]].sort
-        # p "hline: #{hline} #{y} #{x1} #{x2}"
         if x > x1 && x < x2 && y > y1 && y < y2
-          result << {x, y}
+          result << {x, y, vline[0][2] + (x - hline[0][0]).abs + hline[0][2] + (y - vline[0][1]).abs}
         end
       end
     end
-
-    other.verticals.each do |vline|
-      x = vline[0][0]
-      y1, y2 = [vline[0][1], vline[1][1]].sort
-
-      # p "vline: #{vline} #{x} #{y1} #{y2}"
-      @horizonatls.each do |hline|
-        y = hline[0][1]
-        x1, x2 = [hline[0][0], hline[1][0]].sort
-        # p "hline: #{hline} #{y} #{x1} #{x2}"
-        if x > x1 && x < x2 && y > y1 && y < y2
-          result << {x, y}
-        end
-      end
-    end
-
     result
   end
 
@@ -147,10 +165,9 @@ class Grid
     sets = intersections
     return nil if sets.size == 0
     coord = sets.first
-    min = coord[0].abs + coord[1].abs
+    min = coord[2]
     sets.each do |coord|
-      dist = coord[0].abs + coord[1].abs
-      min = dist if min > dist && dist > 0
+      min = coord[2] if min > coord[2] && coord[2] > 0
     end
     min
   end
