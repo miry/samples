90c0a9370cac6eee8d37134e2bd533c98c50f50e
Michael Nikitochkin <miry.sof@gmail.com>
2020-01-23 10:19:30 +0100

    Cosmetic changes: remove extra space in output

--- algorithms/problems/interviewcake/stolen_breakfast.rb
+++ algorithms/problems/interviewcake/stolen_breakfast.rb
@@ -3,10 +3,10 @@
 def stolen_breakfast(list)
   result = 0
   list.each do |item|
-    puts "item: #{item} /  #{item.to_s(2)}"
+    puts "item: #{item} / #{item.to_s(2)}"
     puts "result: #{result.to_s(2)}"
     result ^= item
     puts "xor: #{result.to_s(2)}"
   end
-  return result
+  result
 end
