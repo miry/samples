15c17fcb3899099c41df58cda7ccc15ffc6a0afb
Michael Nikitochkin <miry.sof@gmail.com>
2021-04-17 21:50:30 +0200

    Some work not finished with opentelemtry

--- opentelemetry/go.mod
+++ opentelemetry/go.mod
@@ -2,4 +2,9 @@ module github.com/miry/samples/opentelemetry
 
 go 1.14
 
-require go.opentelemetry.io/otel v0.13.1-0.20201027223049-d98ad1042ccf
+require (
+	go.opentelemetry.io/otel v0.13.0
+	go.opentelemetry.io/otel/exporters/otlp v0.13.0
+	go.opentelemetry.io/otel/sdk v0.13.0
+	google.golang.org/grpc v1.33.1
+)
--- opentelemetry/main.go
+++ opentelemetry/main.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout"
 	"go.opentelemetry.io/otel/global"
+	"go.opentelemetry.io/otel/propagators"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/semconv"
 )
 
 var tracer otel.Tracer
@@ -19,6 +23,10 @@ func main() {
 }
 
 func run() error {
+
+	shutdown := initProvider()
+	defer shutdown()
+
 	ctx := context.Background()
 	tracer = global.Tracer("example")
 	_, span := tracer.Start(ctx, "main")
@@ -43,4 +51,35 @@ func request(ctx context.Context) error {
 	resp, err := http.Get("http://example.com/")
 	log.Println("GET http://example.com/ -> ", resp.StatusCode)
 	return err
+}
+
+func initProvider() func() {
+	exp, err := stdout.NewExporter()
+	handleErr(err, "failed to create exporter")
+
+	bsp := sdktrace.NewBatchSpanProcessor(exp)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+		sdktrace.WithResource(resource.New(
+			// the service name used to display traces in backends
+			semconv.ServiceNameKey.String("test-service"),
+		)),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+
+	// set global propagator to tracecontext (the default is no-op).
+	global.SetTextMapPropagator(propagators.TraceContext{})
+	global.SetTracerProvider(tracerProvider)
+
+	return func() {
+		ctx := context.Background()
+		handleErr(tracerProvider.Shutdown(ctx), "failed to shutdown provider")
+		handleErr(exp.Shutdown(ctx), "failed to stop exporter")
+	}
+}
+
+func handleErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %v", message, err)
+	}
 }
