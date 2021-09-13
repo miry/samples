ef3b888b53a2e580140fe46f29491e266718419c
Michael Nikitochkin <miry.sof@gmail.com>
2021-09-13 17:37:59 +0200

    WenigZeit: Initialized the basic SwiftUI app
    Start a new Xcode project with SwiftUI.
    
    Created a MVC folders similar to Rails.
    
    Update settings to support ios and macos.

---
+++ swift/WenigZeit/WenigZeit/WenigZeitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WenigZeitApp.swift
+//  WenigZeit
+//
+//  Created by Michael Nikitochkin on 13.09.21.
+//
+
+import SwiftUI
+
+@main
+struct WenigZeitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/WenigZeit/WenigZeit/views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WenigZeit
+//
+//  Created by Michael Nikitochkin on 13.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
